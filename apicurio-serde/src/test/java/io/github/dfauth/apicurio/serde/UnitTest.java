a6c1ae5c70499bba94882af6d8e799ecdcf37a91
damian <dfauth@gmail.com>
2025-01-05 11:56:34 +0800

    apicurio example

---
+++ apicurio-serde/src/test/java/io/github/dfauth/apicurio/serde/UnitTest.java
@@ -0,0 +1,45 @@
+package io.github.dfauth.apicurio.serde;
+
+import io.apicurio.registry.serde.SerdeConfig;
+import io.apicurio.registry.serde.avro.AvroSerde;
+import io.github.dfauth.embedded.kafka.test.FavouriteColour;
+import io.github.dfauth.embedded.kafka.test.User;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+@Slf4j
+public class UnitTest {
+
+    private static final String TOPIC = User.class.getCanonicalName();
+    private static final User TEST = io.github.dfauth.embedded.kafka.test.User.newBuilder().setId(1).setUserId("fred").setFavoriteColor(FavouriteColour.RED).build();
+    private Map<String, ?> config = Map.of(
+            SerdeConfig.REGISTRY_URL, "http://localhost:8080/apis/registry/v2",
+            "apicurio.registry.use-specific-avro-reader", true,
+            "apicurio.registry.auto-register", true
+    );
+
+    @Test
+    public void testIt() {
+        try {
+            var serde = new AvroSerde<User>();
+            serde.configure(config, false);
+            try(serde) {
+                var serializer = serde.serializer();
+                var bytes = serializer.serialize(TOPIC, TEST);
+                assertNotNull(bytes);
+                assertTrue(bytes.length > 0);
+                var user = serde.deserializer().deserialize(TOPIC, bytes);
+                assertNotNull(user);
+                assertEquals(TEST, user);
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+}
--- test-application/src/test/java/io/github/dfauth/embedded/kafka/ApicurioAvroConfig.java
+++ test-application/src/test/java/io/github/dfauth/embedded/kafka/ApicurioAvroConfig.java
@@ -1,7 +1,17 @@
 package io.github.dfauth.embedded.kafka;
 
+import io.apicurio.registry.resolver.ParsedSchema;
+import io.apicurio.registry.resolver.SchemaLookupResult;
+import io.apicurio.registry.resolver.SchemaParser;
+import io.apicurio.registry.resolver.SchemaResolver;
+import io.apicurio.registry.resolver.data.Record;
+import io.apicurio.registry.resolver.strategy.ArtifactReference;
+import io.apicurio.registry.resolver.strategy.ArtifactReferenceResolverStrategy;
+import io.apicurio.registry.rest.client.RegistryClient;
 import io.apicurio.registry.serde.avro.AvroKafkaDeserializer;
 import io.apicurio.registry.serde.avro.AvroKafkaSerializer;
+import io.apicurio.registry.serde.avro.AvroSchemaParser;
+import io.apicurio.registry.serde.avro.DefaultAvroDatumProvider;
 import io.github.dfauth.embedded.kafka.test.User;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Deserializer;
@@ -13,6 +23,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Supplier;
@@ -62,6 +74,43 @@ public class ApicurioAvroConfig {
     @Bean
     public AvroKafkaDeserializer avroDeserializer(@Qualifier("serde.config") Map<String, Object> config) {
         AvroKafkaDeserializer deserializer = new AvroKafkaDeserializer();
+        SchemaResolver resolver = new SchemaResolver() {
+            private Map<ArtifactReference, SchemaLookupResult> cache = new HashMap();
+            private ArtifactReferenceResolverStrategy strategy;
+
+            @Override
+            public void setClient(RegistryClient registryClient) {
+            }
+
+            @Override
+            public void setArtifactResolverStrategy(ArtifactReferenceResolverStrategy artifactReferenceResolverStrategy) {
+                this.strategy = artifactReferenceResolverStrategy;
+            }
+
+            @Override
+            public SchemaParser getSchemaParser() {
+                return new AvroSchemaParser(new DefaultAvroDatumProvider());
+            }
+
+            @Override
+            public SchemaLookupResult resolveSchema(Record record) {
+                return cache.get(strategy.artifactReference(record, (ParsedSchema) record.payload()));
+            }
+
+            @Override
+            public SchemaLookupResult resolveSchemaByArtifactReference(ArtifactReference artifactReference) {
+                return cache.get(artifactReference);
+            }
+
+            @Override
+            public void reset() {
+            }
+
+            @Override
+            public void close() throws IOException {
+            }
+        };
+        deserializer.setSchemaResolver(resolver);
         deserializer.configure((Map<String, ?>) config.get("serdeConfig"), false);
         return deserializer;
     }
